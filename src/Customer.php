e865faeb66aaea65b487e4d5bfbbff4f5bd1abbb
edoro <erdoronal28@gmail.com>
2023-05-25 11:57:38 +0700

    Belajar Composer

---
+++ src/Customer.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Edoro\Belajar;
+
+class Customer{
+    public function __construct(private string $name){}
+
+    public function sayHello(string $name) : string{
+        return "Hello $name My Name is $this->name";
+    }
+}
